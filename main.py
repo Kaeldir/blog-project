33fdb2c77a77c2bd3ce17f5079b3fcbbfe8ec421
Kaeldir <legal.mikael@gmail.com>
2021-09-20 09:48:30 +0200

    Fix bleach() function and clean database

--- main.py
+++ main.py
@@ -232,8 +232,23 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-def clean_text(text: str):
-    cleaned_text = bleach.clean(text=text)
+def clean_text(text):
+    allowed_tags = ['a', 'abbr', 'acronym', 'address', 'b', 'br', 'div', 'dl', 'dt',
+                    'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+                    'li', 'ol', 'p', 'pre', 'q', 's', 'small', 'strike',
+                    'span', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+                    'thead', 'tr', 'tt', 'u', 'ul']
+
+    allowed_attrs = {
+        'a': ['href', 'target', 'title'],
+        'img': ['src', 'alt', 'width', 'height'],
+    }
+
+    cleaned_text = bleach.clean(text,
+                                tags=allowed_tags,
+                                attributes=allowed_attrs,
+                                strip=True)
+
     return cleaned_text
 
 
